27249e6371c678aa8c1c7d422384d015618c685e
Pawel Wilkosz-PQM648 <pawel.wilkosz@gmail.com>
2021-07-21 16:13:02 +0200

    Homework

---
+++ AI/week_7/hw_week_7_kaggle_competition.py
@@ -0,0 +1,25 @@
+import pandas
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+from sklearn.neural_network import MLPClassifier
+
+df = pandas.read_csv("C:\Personal\Data\M7\Churn_Modelling.csv", sep=",", header=0);
+
+df.head()
+df.describe()
+
+X = df.iloc[:,0:12]
+Y = df.iloc[:,13]
+X2 = pandas.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(X2, Y, random_state=1)
+
+scaler = StandardScaler()
+scaler.fit(X_train)
+
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test)
+
+mlp = MLPClassifier(hidden_layer_sizes=(11))
+mlp.fit(X_train,y_train)
+mlp.score(X_test, y_test)
